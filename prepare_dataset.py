31089da60e0050f0df15eed161b3d5efa5492d69
x.adam.bednarski@gmail.com <x.adam.bednarski@gmail.com>
2024-10-06 20:51:43 +0200

    sample preparing

---
+++ prepare_dataset.py
@@ -0,0 +1,14 @@
+import random
+SEED = 42
+
+
+if __name__ == "__main__":
+    random.seed(SEED)
+    file = open("task_6-1_all.txt", "r")
+    all_text = file.read().split("\n")
+    samples_ids = random.sample(range(0, len(all_text)), 100)
+    sample_file = open("task_6-1_sample.txt", "w")
+    for i in samples_ids:
+        sample_file.write(all_text[i] + "\n")
+    sample_file.close()
+    file.close()
