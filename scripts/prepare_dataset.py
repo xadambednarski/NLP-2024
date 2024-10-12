06f218a7fdb2cc1fec09968aa0116e42283c3672
x.adam.bednarski@gmail.com <x.adam.bednarski@gmail.com>
2024-10-12 17:50:37 +0200

    new sample

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -1,13 +1,13 @@
 import random
-SEED = 3916111
+SEED = 1312154257
 
 
 if __name__ == "__main__":
     random.seed(SEED)
-    file = open("task_6-1_all.txt", "r")
+    file = open("../data/input/task_6-1_all.txt", "r")
     all_text = file.read().split("\n")
     samples_ids = random.sample(range(0, len(all_text)), 100)
-    sample_file = open("task_6-1_sample.txt", "w")
+    sample_file = open("../data/input/task_6-2_sample.txt", "w")
     for i in samples_ids:
         sample_file.write(all_text[i] + "\n")
     sample_file.close()
